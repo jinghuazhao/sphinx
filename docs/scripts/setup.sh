4afcd44e303b14f009588d3bceb59c0b6c072a18
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-08-28 14:28:59 +0100

    docs

---
+++ docs/scripts/setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+cd ~/rds/software
+module load ceuadmin/python
+python --version
+cd
+python -m venv python3.12
+source python3.12/bin/activate
+pip install sphinx
+mkdir sphinx
+cd sphinx/
+echo Sphinx > README.md
+module load ceuadmin/cli
+git init && git add . && git commit -m "Initial commit"
+gh repo create sphinx   --description "Sphinx project" --public   --source=.   --remote=origin --push
+sphinx-quickstart docs
+make html
