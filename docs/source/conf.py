8bd5e55a9246595acab5e13b20779c1678584ab7
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-08-28 16:46:49 +0100

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,12 +29,16 @@ exclude_patterns = []
 
 # html_theme = 'alabaster'
 import sphinx_rtd_theme
+html_logo = "_static/bees.jpg"
 html_theme = "sphinx_rtd_theme"
+
+# Optionally:
+extensions.append("sphinx_rtd_theme")
+html_static_path = ['_static']
+
 html_theme_options = {
+    'logo_only': True,
     'collapse_navigation': False,
     'navigation_depth': 4,
     'sticky_navigation': True,
 }
-# Optionally:
-extensions.append("sphinx_rtd_theme")
-html_static_path = ['_static']
