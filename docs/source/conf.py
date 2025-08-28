3a62a37c0555101a3c8a13e8342cd8d0e4819af9
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2025-08-28 22:13:49 +0100

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,8 +23,6 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
@@ -35,3 +33,4 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
 html_logo = "_static/bees.jpg"
 html_css_files = ['custom.css']
+html_js_files = ['custom.js']
